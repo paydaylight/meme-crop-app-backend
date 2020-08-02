c9880ea728090ab1ffd3b9f4b312005e5018f300
paydaylight <maxat235@gmail.com>
2020-08-02 15:00:04 +0600

    fix serializer url

--- serializers/image_serializer.py
+++ serializers/image_serializer.py
@@ -4,6 +4,6 @@ from flask import jsonify,request
 def serialize_image(img):
     return jsonify(id=img.id,
                    caption=img.caption,
-                   url=f"{request.url_root}/{img.url}",
+                   url=f"{request.url_root}{img.url}",
                    finished=img.finished,
                    created_at=img.created_at)
